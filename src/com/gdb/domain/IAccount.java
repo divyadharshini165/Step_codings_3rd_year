ece7b4233d0013bcf22b716a339a65eb7dd5c572
divyadharshini165 <vijayalakshmi3181@gmail.com>
2026-09-12 14:38:09 +0530

    Add Activity 11 Interface and Factory Pattern

---
+++ src/com/gdb/domain/IAccount.java
@@ -0,0 +1,28 @@
+package com.gdb.domain;
+
+import com.gdb.exceptions.*;
+
+public interface IAccount {
+
+    String getAccountNumber();
+
+    String getName();
+
+    double getBalance();
+
+    String getAccountType();
+
+    String getStatus();
+
+    int getAge();
+
+    boolean validatePin(String enteredPin);
+
+    boolean changePin(String oldPin, String newPin);
+
+    void deposit(double amount) throws InvalidAmountException;
+
+    void withdraw(double amount, String enteredPin) throws AccountException;
+
+    void displayAccountInfo();
+}
